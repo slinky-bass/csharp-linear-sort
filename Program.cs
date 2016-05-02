f82f34e921da8b79491a557f7dc93abbb2d4ccf7
slinky-bass <danielle.lisa.eriksen@gmail.com>
2016-05-02 19:18:46 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Slinky_Bass
+ * Date: 2016/04/04
+ * Time: 09:58 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+ 
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace linear_sort
+{
+	
+	class Program
+	{	
+	
+		public static void Main()
+		{	
+			string line;
+			StreamReader OriginalCatalogue = new StreamReader(@"c:\Sites\Mus777.cat");
+			StreamWriter ReorderedCatalogue = new StreamWriter(@"c:\Sites\reorder.cat");
+			List<KeyValuePair<string, string>> BarcodeList = new List<KeyValuePair<string, string>>();
+			SortIt BarcodeSort = new SortIt(); //<--- mah very own class, instantiated!!!111!
+		
+			while((line = OriginalCatalogue.ReadLine()) != null)
+			{		
+				if (line != "") 
+				{	
+					string Barcode = line.Substring(15, 13);	
+					BarcodeList.Add(new KeyValuePair<string, string>(Barcode, line));
+				}
+			}
+			
+			BarcodeSort.InsertionSort(BarcodeList);
+			
+			//Console.WriteLine("test");
+			//Console.WriteLine("Barcode: {0} Line: {1}", Barcode, line);
+			
+			foreach (KeyValuePair<string, string> kvp in BarcodeSort.SortedResult) 
+			{
+				ReorderedCatalogue.WriteLine(kvp.Value);
+			}
+			
+			OriginalCatalogue.Close();
+			ReorderedCatalogue.Close();
+		}	
+	}		
+}
---
+++ SortIt.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Slinky_Bass
+ * Date: 2016/04/30
+ * Time: 01:48 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace linear_sort
+{
+	/// <summary>
+	/// SortIt() class performs an insertion sort on a dictionary object and has the property SortedResult
+	/// </summary>
+	public class SortIt
+	{
+		
+		public List<KeyValuePair<string, string>> SortedResult = new List<KeyValuePair<string, string>>();
+			
+		//constructor, this is the thing that is called to instantiate the object LinearSort	
+		public SortIt()
+		{
+			
+		}	
+		
+		//insertionSort method
+		public void InsertionSort(List<KeyValuePair<string, string>> BarcodeList)
+		{			
+					
+			int SourceLength = BarcodeList.Count;
+				
+			//for every item in the source list:
+		    for (int i = 0; i < SourceLength; i++)
+		    {
+		     	//get the key to sort on
+				string SortKey = BarcodeList[i].Key;
+				
+		     	//if the new list is empty, add item
+		     	if (SortedResult.Count == 0) 
+		     	{
+		     		SortedResult.Add(BarcodeList[i]);
+		     	}
+		     	
+		     	else
+		     	{
+		     		int TargetLength = SortedResult.Count;
+		     		
+		     		//compare to each item in the target list
+		     		for (int j = 0; j < TargetLength; j++)
+		     		{
+		     		 	//if the target item is bigger, put new item before it
+		     		 	if (SortedResult[j].Key.CompareTo(SortKey) > 0)
+			     		{
+			     			SortedResult.Insert(j, BarcodeList[i]);	
+			     			break;
+			     		}
+			     		
+			     		//if at the end of the target list, add new item to end of the list
+			     		else if (SortedResult.Count == j-1)
+			     		{
+			     			SortedResult.Add(BarcodeList[i]);
+			     		}
+		     		}	
+		     	}		     	
+		    }
+		}		
+	}
+}
